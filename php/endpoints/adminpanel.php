6b553f6e2e3d991dff86ae646465e2dec33b64c9
Nils Schneider-Sturm <nils.schneider-sturm@htl.rennweg.at>
2020-11-18 10:22:17 +0100

    created admin panel

---
+++ php/endpoints/adminpanel.php
@@ -0,0 +1,18 @@
+<?php
+    require_once "../user.php";
+
+    $user = get_signed_in_user();
+
+    if ($_GET["action"] == "start_contest") {
+
+    } else if ($_GET["action"] == "stop_contest") {
+
+    } else if ($_GET["action"] == "unapproved_image") {
+
+    } else if ($_GET["action"] == "approve_image") {
+
+    } else if ($_GET["action"] == "delete_user") {
+
+    } else if ($_GET["action"] == "delete_photo") {
+
+    }
