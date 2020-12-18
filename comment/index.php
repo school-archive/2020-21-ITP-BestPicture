5d706253b59cdb84264a5c9314a4754c25c5ce0c
Nicoletta2003-1 <71252015+Nicoletta2003-1@users.noreply.github.com>
2020-12-18 21:28:07 +0100

    fixed link

--- comment/index.php
+++ comment/index.php
@@ -22,7 +22,7 @@
         <div class="wrapper">
             <a href="../index.php"> <img class="logo" src="../assets/images/Logo.png" alt="logo"></a>
             <nav>
-                <a href="../index.php">Home</a> <a href="#">Anmelden</a> <a href="../upload/index.php">Upload</a> <a href="../profil/index.php"> <img class="user" src="../assets/images/user.png" alt="user"> </a>
+                <a href="../index.php">Home</a> <a href="../login/login.html">Anmelden</a> <a href="../upload/index.php">Upload</a> <a href="../profil/index.php"> <img class="user" src="../assets/images/user.png" alt="user"> </a>
             </nav>
 
         </div>
@@ -41,7 +41,7 @@
                     $beschreibung = $photo['description'];
                     $likes = get_count_likes_by_photo($id);
 
-                    echo " <img src='$path' id='upload' alt='$namePhoto'>"; #id bild id
+                    echo " <img src='$path' id='upload' alt='$namePhoto'>";
                 ?>
 
             </article>
