9d85e4c5accead5d0fbaa22396519e059371b741
kopsi1209 <71011485+kopsi1209@users.noreply.github.com>
2021-01-13 09:19:45 +0100

    added agbs

--- rechte/rechte.php
+++ rechte/rechte.php
@@ -737,6 +737,49 @@ require_once "../php/user.php";
         erfolgt automatisch mit dem Ausloggen.&nbsp;&nbsp;</p>
     <p>IP-Adressen mit Speicherung von Datum und Uhrzeit des letzten Logins werden nach Ablauf von 7 Tagen von uns gel&ouml;scht.</p>
 
+    <h2>Nutzungsbedingungen</h2>
+    <p>
+
+        Diese Allgemeinen Geschäftsbedingungen ("AGB") gelten für die Website BestPicture (die "Website"), sowie für zugehörige Software, Mobile Apps und weitere im Zusammenhang mit BestPicture stehenden Websites (zusammengefasst der "Service"). Die AGB regeln das Verhältnis zwischen uns, den Betreibern der Website ("Wir" oder "BestPicture") und dem Nutzer ("Du" oder "Nutzer"), aber auch das Verhältnis der Nutzer untereinander.
+
+        "Inhalt(e)" beschreibt hierin Bilder, Videos und Audio-Dateien, die unter der BestPicture Lizenz zur Verfügung stehen. Der Begriff "Bild(er)" umfasst Fotos, Vektorgrafiken, Zeichnungen und Illustrationen. "Video(s)" steht für bewegte Bilder, Animationen, Filmmaterial und andere audiovisuelle Inhalte. Audio-Dateien umfasst Soundeffekte, weitere Klänge, Musikdateien und Ähnliches.
+
+        BestPicture kann diese Bedingungen jederzeit und ohne Ankündigung anpassen. Das Datum der letzten Änderung ist am Anfang dieser Seite genannt. Durch den weiteren Einsatz geben die Nutzer ihr Einverständnis zu den vorgenommenen Änderungen.
+        Voraussetzungen und Anmeldung
+
+        Du darfst den Service nur nutzen, wenn Du in Bezug auf diese AGB einen rechtlich bindenden Vertrag mit BestPicture eingehen kannst. Wenn Du unter 16 Jahre alt bist, darfst Du BestPicture nur mit Zustimmung Deiner Eltern, Deines Erziehungsberechtigten oder Deines Lehrer nutzen. Registrierungsdaten und weitere Informationen, die Du über den Service mitteilst, unterliegen unseren Datenschutzrichtlinien.
+        Nutzung des Service
+
+        Data Mining, Scraping und anderweitiger Einsatz von Programmen zur automatischen Datenerfassung und/oder Extraktion digitaler Daten ist strengstens untersagt. Die Technologie und Software, die dem Service zugrunde liegt, ist Eigentum von BestPicture und unseren Lizenzgebern, verbundenen Unternehmen und Partnern. Eine Lizenz hierfür wird nicht gewärt. Die Software darf weder im Ganzen noch in Teilen kopiert, wiederverwertet, nachkonstruiert und/oder dekompiliert werden - es sei denn BestPicture erteilt dazu die Genehmigung.
+
+        Das Massenkopieren von Inhalten ist strikt untersagt sofern keine explizite Erlaubnis durch BestPicture erteilt wurde. Dies betrifft alle Inhalte auf BestPicture, inklusive der unter der BestPicture Lizenz veröffentlichten Bilder, Videos und Audio-Dateien. Die Zusammenstellung der Inhalte ist als kollektives Werk entsprechend internationaler Konventionen und Gesetze zum geistigen Eigentum urheberrechtlich geschützt.
+
+        BestPicture Lizenz – Nutzung der Inhalte
+
+        Inhalte auf BestPicture werden unter der BestPicture Lizenz mit den folgenden Bedingungen zur Verfügung gestellt. Durch die BestPicture Lizenz erhältst Du ein unwiderrufliches, weltweites, nicht exklusives und gebührenfreies Recht, die Inhalte für kommerzielle und nicht kommerzielle Zwecke zu verwenden, herunterzuladen, zu kopieren und zu verändern. Eine Nennung des Künstlers/Urhebers bzw. von BestPicture ist nicht erforderlich, wir wissen jedoch eine freiwillige Quellenangabe zu schätzen.
+
+        Die BestPicture-Lizenz gestattet nicht:
+
+        den Verkauf oder Vertrieb von Inhalten in digitaler Form, insbesondere als Stockfotos oder digitale Wallpaper;
+        den Verkauf oder Vertrieb von Inhalten z.B. als Poster, Digitaldrucke, Musiktitel oder physische Produkte, ohne zusätzliche Elemente hinzuzufügen oder anderweitig einen Mehrwert zu schaffen;
+        die Darstellung von identifizierbaren Personen auf beleidigende, pornografische, obszöne, unmoralische, diffamierende oder verleumderische Weise; oder
+        die Suggestion, dass abgebildete Personen, Marken, Organisationen, etc. bestimmte Produkte oder Dienstleistungen befürworten oder billigen, es sei denn es wurde eine Genehmigung dazu erteilt.
+
+        Beachte bitte, dass alle Inhalte auf BestPicture zwar für kommerzielle und nicht-kommerzielle Zwecke frei verwendbar sind, dargestellte Elemente in den Inhalten, wie identifizierbare Personen, Logos und Marken, jedoch zusätzlichen Urheberrechten, Eigentumsrechten, Personenrechten, Markenrechten usw. unterliegen können. Die Zustimmung eines Dritten oder die Lizenz dieser Rechte können insbesondere für kommerzielle Anwendungen erforderlich sein. BestPicture garantiert nicht, dass solche Zustimmungen oder Lizenzen eingeholt wurden, und lehnt ausdrücklich jegliche Haftung in dieser Hinsicht ab.
+        Hochladen von Inhalten
+
+        Durch das Hochladen von Inhalten auf die Website gewährst Du BestPicture ein unwiderrufliches, weltweites, nicht-exklusives und gebührenfreies Recht, die Inhalte für kommerzielle als auch für nicht-kommerzielle Zwecke zu verwenden, herunterzuladen, zu kopieren oder zu verändern. Dies beinhaltet auch das Recht von BestPicture, die Inhalte unter der BestPicture Lizenz - oder einer anderen von BestPicture angebotenen Lizenz - weiterzugeben.
+
+        Du bist für Deine hochgeladenen Inhalte selbst verantwortlich und garantierst,
+
+        dass Du alle Rechte an den Inhalten besitzt, die Du auf der Website hochlädst, und dass die Inhalte nicht gegen das Urheberrecht, Eigentumsrecht, Markenrecht oder andere anwendbare Rechte Dritter verstoßen; und
+        dass Du alle Rechte am Inhalt, die für eine weltweite, kostenlose, unwiderrufliche und zeitlich unbegrenzte Nutzung erforderlich sind, besitzt und diese - sofern gesetzlich möglich - abtrittst und/oder weitergibst. Hierzu gehören insbesondere gültige Modelverträge, Fotografiererlaubnis, sowie weitere Lizenzen und Rechte Dritter.
+
+        Obwohl BestPicture strikt darauf achtet, dass hochgeladene Inhalte nicht missbraucht bzw. entgegen unserer Nutzungsbedingungen eingesetzt werden, können wir dies leider nicht garantieren und im gegebenen Falle auch nicht zur Verantwortung gezogen werden.
+
+        Wir behalten uns das Recht vor, jeden Inhalt auf BestPicture jederzeit zu löschen. Dies trifft insbesondere auf defekte Dateien, auf Inhalte von minderer Qualität sowie auf rechtlich Problematisches zu.
+
+    </p>
 
 </section>
 
