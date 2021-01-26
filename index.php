449a8e1bd3295d1e0bb1fee43990a2fab16e3b0d
kopsi1209 <71011485+kopsi1209@users.noreply.github.com>
2021-01-26 07:59:37 +0100

    changed countdown

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ require_once "php/user.php";
                 hour = minute * 60,
                 day = hour * 24;
 
-            let birthday = "Jan 24, 2021 00:00:00",
+            let birthday = "Feb 07, 2021 00:00:00",
                 countDown = new Date(birthday).getTime(),
                 x = setInterval(function () {
 
